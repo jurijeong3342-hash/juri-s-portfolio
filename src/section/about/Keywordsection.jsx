a0f3d3d483c84017950cd051c615ee6fa77ad4a8
juri <0123914@naver.com>
2026-02-10 14:54:19 +0900

    ..

--- src/section/about/Keywordsection.jsx
+++ src/section/about/Keywordsection.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import "./KeywordSection.css";
+import "./Keywordsection.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
